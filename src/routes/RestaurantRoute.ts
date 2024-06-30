1dd0052a345d67253b192edf48105d8903b1504c
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-06-30 13:36:03 -0300

    add user, order and restaurant route

---
+++ src/routes/RestaurantRoute.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { param } from "express-validator";
+import RestaurantController from "../controllers/RestaurantController";
+
+const router = express.Router();
+
+router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("RestaurauntId parameter must be a valid string"),
+  RestaurantController.getRestaurant
+);
+
+router.get(
+  "/search/:city",
+  param("city")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("City parameter must be a valid string")
+);

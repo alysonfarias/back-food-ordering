afe62a4465c2589bea87e964d222cd4f9c2ed216
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-08-10 08:50:15 -0300

    fix: restaurant model

--- src/models/restaurant.ts
+++ src/models/restaurant.ts
@@ -13,13 +13,13 @@ const menuItemSchema = new mongoose.Schema({
 export type MenuItemType = InferSchemaType<typeof menuItemSchema>;
 
 const restaurantSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
-  restaurantName: { type: mongoose.Schema.Types.String, required: true },
-  city: { type: mongoose.Schema.Types.String, required: true },
-  country: { type: mongoose.Schema.Types.String, required: true },
-  deliveryPrice: { type: mongoose.Schema.Types.Number, required: true },
-  estimatedDeliveryTime: { type: mongoose.Schema.Types.Number, required: true },
-  cuisines: { type: mongoose.Schema.Types.String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  restaurantName: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+  deliveryPrice: { type: Number, required: true },
+  estimatedDeliveryTime: { type: Number, required: true },
+  cuisines: [{ type: String, required: true }],
   menuItems: [menuItemSchema],
   imageUrl: { type: String, required: true },
   lastUpdated: { type: Date, required: true },
